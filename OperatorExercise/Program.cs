24c8b9b3aca99782c13bcdec9a87b3a8bc1cab25
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-15 20:05:36 -0400

    Resubmitted

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,6 +8,12 @@ namespace OperatorExercise
         static void Main(string[] args)
         {
             //Exercise 1
+            int add = 1 + 1;
+            int subtract = 1 - 1;
+            int multiply = 1 * 1;
+            int divide = 1 / 1;
+            int modulo = 1 % 1;
+
             int a = 17;
             int b = 4;
 
@@ -16,6 +22,26 @@ namespace OperatorExercise
 
             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
 
+            //Exerceise 2
+
+            Console.WriteLine(“What is the radius of your circle ?”);
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            AreaOfCircle(radius);
+
+            Console.WriteLine($"A circle with a radius of {radius} has an area of {AreaOfCircle()}");
+
+
+            //Thought Exercise
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+            Console.WriteLine(k);
+
+            /*
             //Exercise 2
             Console.WriteLine("What is the radius of your circle?");
             var radius = double.Parse(Console.ReadLine());
@@ -24,14 +50,17 @@ namespace OperatorExercise
 
 
             Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+            */
 
         }
         
         public static double AreaOfCircle(double radius)
         {
-            var area = (Math.PI * radius * radius);
+            var area = (Math.PI * Math.Pow(radius, 2);
+
             return area;
         }
+        
             
     }
 }
